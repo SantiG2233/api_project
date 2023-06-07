23a97fa245e5c62099df767321c133d89494ba6a
SantiG2233 <67402678+SantiG2233@users.noreply.github.com>
2023-06-07 13:42:39 -0600

    Update app.py

--- app.py
+++ app.py
@@ -10,7 +10,20 @@ app = Flask(__name__)
 def home():
     engine = create_engine('postgresql://postgres:(CIMB2023)@proyectosanti.postgres.database.azure.com:5432/postgres')
     data = request.get_json()
-    time = f"""WITH previous_interval AS (SELECT percentile_cont(0.5) WITHIN GROUP (ORDER BY "Throttle") AS previous_average FROM measurements WHERE measurement_time >= '{data['inicio']}' - INTERVAL '120 second' AND measurement_time < '{data['inicio']}' - INTERVAL '60 second'), current_interval AS (SELECT percentile_cont(0.5) WITHIN GROUP (ORDER BY "Throttle") AS current_average FROM measurements WHERE measurement_time >= '{data['inicio']}' - INTERVAL '60 second') SELECT previous_average, current_average, (current_average - previous_average) / previous_average * 100 AS percentile_difference FROM previous_interval, current_interval;"""
+    #time = f"""SELECT AVG("Throttle") FROM measurements WHERE measurement_time >= '{data['inicio']}' and measurement_time <= '{data['final']}'"""
+    time = f"""WITH previous_interval AS (
+    SELECT percentile_cont(0.5) WITHIN GROUP (ORDER BY "Throttle") AS previous_average
+    FROM measurements
+    WHERE measurement_time >= '{data['inicio']}'::timestamp - INTERVAL '120 second'
+    AND measurement_time < '{data['inicio']}'::timestamp - INTERVAL '60 second'
+    ),
+    current_interval AS (
+    SELECT percentile_cont(0.5) WITHIN GROUP (ORDER BY "Throttle") AS current_average
+    FROM measurements
+    WHERE measurement_time >= '{data['inicio']}'::timestamp - INTERVAL '60 second'
+    )
+    SELECT previous_average, current_average, (current_average - previous_average) / previous_average * 100 AS percentile_difference
+    FROM previous_interval, current_interval;"""
     df = pd.read_sql(sql=time,con=engine)
     previous = df.iloc[0,0]
     current = df.iloc[0,1]
